eb5e8741e1c7ad9b2f51a656335d181aa8a3b682
Daphane <daphane.elizabeth@gmail.com>
2025-02-17 18:33:31 -0500

    112 class 4

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import (
+ListView,
+CreateView,
+DetailView
+)
+
+from .models import Post
+from django.urls import reverse
+
+class PostListView(ListView):
+	template_name = "posts/list.html"
+	model = Post
+	
+class PostDetailView(DetailView):
+	template_name = "posts/detail.html"
+	model = Post
+
+class PostCreateView(CreateView):
+	template_name = "posts/new.html"
+	model = Post
+	fields = ["title", "subtitle", "body"]
+	# success_url = reverse("list")
