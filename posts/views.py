35e1a6af8069baca5c646734aaf28f74fd80fdf1
Daphane <daphane.elizabeth@gmail.com>
2025-02-22 17:59:08 -0500

    class 2

--- posts/views.py
+++ posts/views.py
@@ -5,30 +5,79 @@ ListView,
 DetailView,
 CreateView
 )
-from .models import Post
+from .models import Post, Status
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import (LoginRequiredMixin, UserPassesTestMixin)
 
-class PostUpdateView(UpdateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+	template_name = "posts/new.html"
+	model = Post
+	fields = ["title", "subtitle", "body", "status"]
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		# form.instance.status = Status.objects.get(name="Published")
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
 	template_name = "posts/edit.html"
 	fields = [
-		"title", "subtitle", "body", "author"]
+		"title", "subtitle", "body", "status"]
 
-class PostDeleteView(DeleteView):
+	def test_func(self):
+		post = self.get_object()
+		return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	template_name = "posts/delete.html"
 	model = Post
 	success_url = reverse_lazy("list")
 
+	def test_func(self):
+		post = self.get_object()
+		return self.request.user == post.author
+
 class PostListView(ListView):
 	template_name = "posts/list.html"
 	model = Post
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		Published = Status.objects.get(name="Published")
+		context["post_list"] = (
+			Post.objects.filter(status=Published)
+			.order_by("-created_on").reverse()
+		)
+		return context
+	
+class DraftPostListView(LoginRequiredMixin, ListView):
+	template_name = "posts/list.html"
+	model = Post
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		Draft = Status.objects.get(name="Draft")
+		context["post_list"] = (
+			Post.objects.filter(status=Draft)
+			.order_by("-created_on").reverse()
+		)
+		return context
+
+class ArchivedPostListView(LoginRequiredMixin, ListView):
+	template_name = "posts/list.html"
+	model = Post	
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		Archived = Status.objects.get(name="Archived")
+		context["post_list"] = (
+			Post.objects.filter(status=Archived)
+			.order_by("-created_on").reverse()
+		)
+		return context
 	
 class PostDetailView(DetailView):
 	template_name = "posts/detail.html"
 	model = Post
 
-class PostCreateView(CreateView):
-	template_name = "posts/new.html"
-	model = Post
-	fields = ["title", "subtitle", "body", "author"]
-	# success_url = reverse("list")
